ff35c59c55ceb184856ca6b6b045bc188749596e
vaishnavi22-s4s <vaishnvi22.s4s@gmail.com>
2024-12-27 10:09:40 +0530

    margin-top change

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -8,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './testimonial.component.scss'
 })
 export class TestimonialComponent {
+  testimonial=[
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    },
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    },
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    },
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    },
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    },   
+    {
+      imageUrl:"testimonial-img.jpg",
+      name:"Sarah. M, Tea Shop Owner",
+      content1:"Butter Biscuit transformed our tea shop!",
+      content2:'"Our customers love the easy QR code access, and we’ve received so many compliments on the visuals. Updating our menu is now a breeze!"',
+    }
+  ]
 
 }
