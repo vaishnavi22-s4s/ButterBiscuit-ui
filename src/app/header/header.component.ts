191bfd866c73f1312c63bd6aad51683c5c85de1b
Swathilakshmi16 <swathilakshmi.m.s4s@gmail.com>
2024-12-27 17:31:52 +0530

    responsive1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
