12299267c7ec24df447e8eec83ac271393187f2f
vaishnavi22-s4s <vaishnvi22.s4s@gmail.com>
2024-12-28 10:06:33 +0530

    our-partner

--- src/app/our-partner/our-partner.component.ts
+++ src/app/our-partner/our-partner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { withDebugTracing } from '@angular/router';
 
 @Component({
@@ -8,41 +8,84 @@ import { withDebugTracing } from '@angular/router';
   templateUrl: './our-partner.component.html',
   styleUrl: './our-partner.component.scss'
 })
-export class OurPartnerComponent {
-  ourPartner=[
-    {
-      imageUrl:"partner-img1.jpeg",
-      width:"260px",
-      height:"100px"
-    },
-    {
-      imageUrl:"partner-img2.jpeg",
-      width:"260px",
-      height:"100px"
-    },
-    {
-      imageUrl:"partner-img3.jpeg",
-      width:"260px",
-      height:"100px"
-    },
-    {
-      imageUrl:"partner-img4.jpeg",
-      width:"260px",
-      height:"100px"
-    },
-    {
-      imageUrl:"partner-img5.jpeg",
-      width:"200px",
-      height:"150px"
-    },
-    {
-      imageUrl:"partner-img6.jpeg",
-      width:"200px",
-      height: "150px"
-    }
+export class OurPartnerComponent implements OnInit{
+  // ourPartner=[
+  //   {
+  //     imageUrl:"partner-img1.jpeg",
+  //     width:"260px",
+  //     height:"100px"
+  //   },
+  //   {
+  //     imageUrl:"partner-img2.jpeg",
+  //     width:"260px",
+  //     height:"100px"
+  //   },
+  //   {
+  //     imageUrl:"partner-img3.jpeg",
+  //     width:"260px",
+  //     height:"100px"
+  //   },
+  //   {
+  //     imageUrl:"partner-img4.jpeg",
+  //     width:"260px",
+  //     height:"100px"
+  //   },
+  //   {
+  //     imageUrl:"partner-img5.jpeg",
+  //     width:"200px",
+  //     height:"150px"
+  //   },
+  //   {
+  //     imageUrl:"partner-img6.jpeg",
+  //     width:"200px",
+  //     height: "150px"
+  //   }
 
-  ]
+  // ]
+
+  ourPartner = [
+    { imageUrl: 'partner-img1.jpeg', width: '100px', height: '100px' },
+    { imageUrl: 'partner-img2.jpeg', width: '100px', height: '100px' },
+    { imageUrl: 'partner-img3.jpeg', width: '100px', height: '100px' },
+    { imageUrl: 'partner-img4.jpeg', width: '100px', height: '100px' },
+    { imageUrl: 'partner-img5.jpeg', width: '100px', height: '100px' }
+  ];
+
+  visibleImages:any;
+  currentIndex = 0;
+  imagesPerSlide = 3;
+
+  ngOnInit(): void {
+    this.updateVisibleImages();
+  }
+
+  updateVisibleImages(): void {
+    const start = this.currentIndex;
+    const end = this.currentIndex + this.imagesPerSlide;
+    this.visibleImages = this.ourPartner.slice(start, end);
+  }
+
+  nextSlide(): void {
+    this.currentIndex += this.imagesPerSlide;
+    if (this.currentIndex >= this.ourPartner.length) {
+      this.currentIndex = 0; // Loop back to the start
+    }
+    this.updateVisibleImages();
+  }
+
+  prevSlide(): void {
+    this.currentIndex -= this.imagesPerSlide;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.ourPartner.length - this.imagesPerSlide; // Loop to the end
+    }
+    this.updateVisibleImages();
+  }
+
+
+
+
 
 }
+
 
 
