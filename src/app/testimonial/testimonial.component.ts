57b91669d965a3c971d1b94ac43242eaacc45b52
vaishnavi22-s4s <vaishnvi22.s4s@gmail.com>
2025-05-26 23:01:30 +0530

    last changes

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import Splide from '@splidejs/splide';
-import '@splidejs/splide/css';
+
+
 
 @Component({
   selector: 'app-testimonial',
@@ -10,24 +10,10 @@ import '@splidejs/splide/css';
   styleUrl: './testimonial.component.scss',
 
 })
-export class TestimonialComponent implements OnInit {
+export class TestimonialComponent {
 
 
 
-  ngOnInit(): void {
-    new Splide('#splide', {
-      type: 'loop',      // Enables looping of slides
-      perPage: 3,        // Show 3 slides per row
-      perMove: 1,        // Move 1 slide at a time
-      autoplay: true,    // Enable auto-play
-      interval: 3000,    // 3 seconds between slides
-      pauseOnHover: true, // Pause autoplay on hover
-      pagination: true,  // Enable pagination indicators
-      arrows: true,      // Enable navigation arrows
-      gap: '4rem',    
-      focus: 'center',   // Space between slides
-     
-    }).mount();
 
     
   }
@@ -36,5 +22,5 @@ export class TestimonialComponent implements OnInit {
 
   
 
-}
+
 
